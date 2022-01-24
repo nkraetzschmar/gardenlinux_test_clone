43c197bf99878088548fa797867ca87e47e3788f
nkraetzschmar <nkraetzschmar@users.noreply.github.com>
2022-01-24 22:06:01 +0100

    WIP: GitHub actions: upload release

---
+++ .github/workflows/upload_release.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -Eeufo pipefail
+
+token="$1"
+repo="$2"
+tag="$3"
+name="$4"
+
+curl_path="$(which curl)"
+
+function curl {
+	$curl_path -s -f -u "token:$token" "$@"
+}
+
+function get {
+	[ $# = 1 ]
+	curl -X GET "https://api.github.com/repos/$repo/$1"
+}
+
+function post {
+	[ $# = 2 ]
+	curl -X POST "https://api.github.com/repos/$repo/$1" --data "$2"
+}
+
+function delete {
+	[ $# = 1 ]
+	curl -X DELETE "https://api.github.com/repos/$repo/$1"
+}
+
+function upload {
+	[ $# = 1 ]
+	curl -X POST -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$repo/$1" --data-binary @-
+}
+
+release="$(get "releases/tags/$tag" | jq -r '.id' || true)"
+
+[ "$release" ] || release="$(post "releases" '{
+	"tag_name": "'$tag'",
+	"name": "'$name'",
+	"body": "auto release, created by GitHub action"
+}' | jq -r '.id')"
+
+while read asset_file; do
+	asset_name="$(basename "$asset_file")"
+	asset="$(get "releases/$release" | jq -r '.assets[] | select(.name == "'$asset_name'") | .id' || true)"
+	[ ! "$asset" ] || delete "releases/assets/$asset"
+	upload "releases/$release/assets?name=$asset_name" < "$asset_file" > /dev/null
+	echo "uploaded $asset_file to $release"
+done
